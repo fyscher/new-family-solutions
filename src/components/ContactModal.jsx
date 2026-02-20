a7eb42f362ec2dfc253b285c7fa392c07466c6bd
Fyscher Kraal <fyscher.kraal@gmail.com>
2026-02-19 20:32:54 -0700

    add form to modal

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -1,7 +1,16 @@
+import "../static/css/contact.css";
 import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 const ContactModal = ({ onClose }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [, setContact] = useState(false);
+
+  const changeFirst = (e) => setFirstName(e.target.value);
+  const changeLast = (e) => setLastName(e.target.value);
+  const changeEmail = (e) => setEmail(e.target.value);
   return (
     <motion.div
       className="overlay"
@@ -18,7 +27,50 @@ const ContactModal = ({ onClose }) => {
         exit={{ scale: 0.9, opacity: 0 }}
         transition={{ type: "spring", stiffness: 400, damping: 25 }}
       >
-        Hello!
+        <>
+          <div className="i-header">
+            <h2>Connect with us</h2>
+            <h4 className="i-h4">
+              Send us a message. We will contact you as soon as we can.
+            </h4>
+          </div>
+          <form className="contact">
+            <input
+              required
+              placeholder="First Name"
+              type="text"
+              className="first"
+              value={firstName}
+              onChange={changeFirst}
+            />
+            <input
+              placeholder="Last Name (optional)"
+              type="text"
+              className="last"
+              value={lastName}
+              onChange={changeLast}
+            />
+            <br />
+            <input
+              required
+              placeholder="Contact Email"
+              type="email"
+              className="email"
+              value={email}
+              onChange={changeEmail}
+            />
+            <br />
+            <textarea
+              required
+              placeholder="Let us know what you want, and we will get back to you in the email provided above shortly!"
+              className="inquiry"
+            ></textarea>
+            <br />
+            <button type="submit" onClick={() => setContact(true)}>
+              Send
+            </button>
+          </form>
+        </>
         <br />
         <button className="close-button" onClick={onClose}>
           Close
